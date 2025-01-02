c6b2f5edfa6561efba96075be72cbeb25a2b37ab
Md. Sabbir Hossain <cse.170201039@gmail.com>
2025-01-02 19:29:05 +0600

    refac(task_users): add routers for CustomUserRetriveUpdateDestroyApiView

--- Backend/task_users/urls.py
+++ Backend/task_users/urls.py
@@ -1,11 +1,16 @@
 # urls.py
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from task_users.views import UserCreateView, LoginView, CustomUserRetriveUpdateDestroyView
+from task_users.views import UserCreateView, LoginView, CustomUserRetriveUpdateDestroyApiView
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('users', CustomUserRetriveUpdateDestroyApiView, basename='user')
 
 urlpatterns = [
+    path('api/register/', UserCreateView.as_view(), name='create_user'),
     path('api/login/', LoginView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/register/', UserCreateView.as_view(), name='create_user'),
-    path('api/users/<int:pk>', CustomUserRetriveUpdateDestroyView.as_view(), name='user_retrive_update_destroy'),
 ]
+
+urlpatterns += router.urls
